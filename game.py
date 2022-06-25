54ec2730f3d31097889aef980713d2b8c39fea71
Wessley Alberts <wessley.alberts@npbhs.school.nz>
2022-06-25 16:22:24 +1200

    Added game functionallity

--- game.py
+++ game.py
@@ -18,79 +18,104 @@ def game():
             self.vx=vx
             #y distance moved per frame
             self.vy=vy
+            #colour of ball
             self.colour=colour
 
-        def getx(self):
+        def get_x(self):
+            """Gets the x pos of the ball"""
             return self.x
         
-        def gety(self):
+        def get_y(self):
+            """Gets the y pos of the ball"""
             return self.y
         
+        def get_size(self):
+            """Gets the size of the ball"""
+            return self.size
+        
         def move(self):
+            """Moves the ball """
+            #Moves the ball
             self.x+=self.vx
             self.y+=self.vy
-            if self.y+self.size>=300:
-                self.y=300-self.size
+            #Finalizes the movement of the ball
+            canvas.create_oval(self.x,self.y,self.x+self.size,self.y+self.size,fill=self.colour)
+        
+        def collision(self):
+            """Checks if the ball is coliding with another ball or the wall"""
+            #Checks if the ball is coliding with another ball
+            for item in balls:
+                x=item.get_x()
+                y=item.get_y()
+                size=item.get_size()
+                if self.x<x<self.x+self.size or x<self.x<x+size:
+                    if self.y<y<self.y+self.size or y<self.y<y+size:
+                        self.vx=self.vx*-1
+                        self.vy=self.vy*-1
+            #Checks if the ball is coliding with the wall 
+            if self.y+self.size>=screen_height-canvas_height*2:
+                self.y=screen_height-canvas_height*2-self.size
                 self.vy=self.vy*-1
             if self.y<=0:
                 self.y=0
                 self.vy=self.vy*-1
-            if self.x+self.size>=400:
-                self.x=400-self.size
+            if self.x+self.size>=screen_width-canvas_width*2:
+                self.x=screen_width-canvas_width*2-self.size
                 self.vx=self.vx*-1
             if self.x<=0:
                 self.x=0
                 self.vx=self.vx*-1
-            canvas.create_oval(self.x,self.y,self.x+self.size,self.y+self.size,fill=self.colour)
-        
-        def collision(self):
-            for item in balls:
-                if item.getx==self.x+5 and item.gety==self.y+5:
-                    self.vx=self.vx*-1
-                    self.vy=self.vy*-1
     
     
     def makeball(x,y,size,vx,vy,colour):
+        """Creates and stores a ball"""
         ball=Ball(x,y,size,vx,vy,colour)
         balls.append(ball)
 
     def addball():
-        vx=0
-        while vx==0:
-            vx=randint(-10,10)
-            vx=vx/50
-        vy=0
-        while vy==0:
-            vy=randint(-10,10)
-            vy=vy/50
-        makeball(randint(0,400),randint(0,300),10,vx,vy,"red")
+        """Creates the properties of a new ball"""
+        makeball(randint(0,screen_width-round(screen_width/50)),randint(0,screen_height-round(screen_height/50)),50,randint(-10,10)/50,randint(-10,10)/50,"red")
     
     def ball_click(event):
+        """Confirms if the ball was clicked"""
         global x,y,t_end
         x=event.x
         y=event.y
-        if x>balls[0].x and x<balls[0].x+20:
-            if y>balls[0].y and y<balls[0].y+20:
+        if x>balls[0].x and x<balls[0].x+balls[0].size:
+            if y>balls[0].y and y<balls[0].y+balls[0].size:
                 t_end = time() + 5
                 addball()
         
     
-    makeball(50,50,20,0.05,0.05,"blue")
-    
     game=Toplevel()
-    game.geometry("400x400")
-
-    canvas=Canvas(game,width=400,height=300,bg="black")
+    #Sets the balls storage
+    balls=[]
+    #Sets the screen dimensions
+    global screen_width, screen_height, canvas_width, canvas_height
+    screen_width = game.winfo_screenwidth()
+    screen_height = game.winfo_screenheight()
+    canvas_width = int(round(screen_width/100))
+    canvas_height = int(round(screen_height/100))
+    
+    game.title("Game")
+    game.geometry(str(screen_width-canvas_width*2)+"x"+str(screen_height-canvas_height*2)+"+0+0")
+    Frame(game,height=(screen_height/100)).pack()
+    canvas=Canvas(game,width=screen_width-(canvas_width),height=screen_height-(canvas_height),bg="black")
     canvas.pack()
+    
+    #Creates the target ball
+    makeball(randint(0,screen_width-canvas_width*2),randint(0,screen_height-canvas_height*2),100,0.2,0.2,"blue")
+    
     playing=True
+    
     while playing==True:
         global t_end
         t_end = time() + 5
         while time() < t_end:
             canvas.delete(ALL)
             for item in balls:
-                    item.move()
                     item.collision()
+                    item.move()
             canvas.update()
             canvas.bind("<Button 1>", ball_click)
             if time()>=t_end:
@@ -98,8 +123,6 @@ def game():
 
 
 if __name__=="__main__":
-    #Sets the balls storage
-    balls=[]
 
     """Creates the layout of the Main Menu"""
     #Sets the main menu window
